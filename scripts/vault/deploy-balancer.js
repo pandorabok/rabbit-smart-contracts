ab96671d7ab34bf71ce29caee87653ebdc09dc3a
pandorabok <borkovicbiljana9@gmail.com>
2025-04-04 15:09:00 -0700

    Contract development update - added 4 lines

--- scripts/vault/deploy-balancer.js
+++ scripts/vault/deploy-balancer.js
@@ -1,3 +1,7 @@
 import hardhat, { ethers, web3 } from "hardhat";
 import { addressBook } from "blockchain-addressbook";
 import { predictAddresses } from "../../utils/predictAddresses";
+
+const {
+  platforms: { velodrome, rabbityieldfinance },
+  tokens: {
